59a4836501bf4a32aa0fc85a3db198cc0a4e7f88
pavan kumar <36876515+pavants52@users.noreply.github.com>
2018-03-18 13:00:15 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/rou
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './accountApp/loginPage/login.component';
